93c2be8a7a141e2da95b08eb34d23141b09ce774
taisei-sae <saegusa.taisei@gmail.com>
2026-08-06 17:04:49 +0900

    move the parity to right

--- ros2/cepton_publisher/src/cepton_publisher.cpp
+++ ros2/cepton_publisher/src/cepton_publisher.cpp
@@ -498,7 +498,7 @@ CeptonPublisher::CeptonPublisher() : Node("cepton_publisher")
     points_publisher = create_publisher<PointCloud2>("cepton_pcl2", 50);
 
     // Register callback
-    ret = CeptonListenFramesEx(CEPTON_AGGREGATION_MODE_SINGLE_PARITY_LEFT, on_ex_frame, this);
+    ret = CeptonListenFramesEx(CEPTON_AGGREGATION_MODE_SINGLE_PARITY_RIGHT, on_ex_frame, this);
     check_sdk_error(ret, "CeptonListenFramesEx");
   }
   RCLCPP_DEBUG(this->get_logger(), "=================================================");
